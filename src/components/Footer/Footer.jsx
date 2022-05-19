36edf3fc80d2135bc63b1f557f60c7fd6406ec94
alenriquez96 <alberto.enriquez96@gmail.com>
2022-05-19 20:10:18 +0200

    styles home

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import colaboradores from "../../assets/colaboradores.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div id="footer1">
+        <a href="">Aviso Legal</a>
+        <a href="">Política de Privacidad</a>
+        <a href="">Política de cookies</a>
+      </div>
+      <div id="footer2">
+        <img src={colaboradores} alt="colaboradores" />
+        <p>@2022 ENTREFAMILIAS</p>
+      </div>
+    </footer>
+
+  )
 };
 
 export default Footer;
