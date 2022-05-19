1e973bcee90a68bf5c0ca80b41ce62dc84ed436a
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-05-19 18:48:02 +0200

    started home design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Nav from "./Nav";
 import {Link}from "react-router-dom";
+import loginLogo from "../../assets/acc-circle.png"
+import voluntariadoLogo from "../../assets/voluntariado-logo.png"
 
 
 const Header = () => {
   return (
     <header>
-      <Nav/>
-      <div>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Signup</Link>
+      <div id="divLogos">
+        <img src={voluntariadoLogo} alt="voluntariado logo"/>
+        <Link to="/login"><img src={loginLogo} alt="login logo"/></Link>
       </div>
+      <Nav/>
     </header>
   )
 };
--- src/components/Main/Home/Home.jsx
+++ src/components/Main/Home/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import logoGrande from "../../../assets/logoGrande.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <img src={logoGrande} alt="logo grande"/>
+      <article>
+        <h1>Entre Familias</h1>
+        <p>Ayudamos a familias de acogida en el proceso de adaptación del niño en el hogar. Todos nos hemos sentido solos, es el momento de unirse.</p>
+        <button>¡Te estamos esperando!</button>
+      </article>
+    </div>
+  )
 };
 
 export default Home;
