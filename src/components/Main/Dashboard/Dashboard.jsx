a8033546fb8264c8b81b8c0dbc3919cbb9ba94fd
Ricardo <Ricardontutumu@gmail.com>
2022-05-20 07:30:55 +0200

    ADDE & TEST create Entry from Front

--- src/components/Main/Dashboard/Dashboard.jsx
+++ src/components/Main/Dashboard/Dashboard.jsx
@@ -4,10 +4,11 @@ import axios from "axios";
 
 const Dashboard = () => {
   const { register, handleSubmit } = useForm();
-  
-  const createEntry = async (create) =>{
+
+  const createEntry = async (create) => {
+    console.log('funcion front createEntry');
     try {
-      const res = await axios.post("http://localhost:5000/api/users/create", create);
+      const res = await axios.post("http://localhost:5000/api/dashboard/blog/create", create);
       const data = res.data;
       console.log(data);
     } catch (error) {
@@ -19,7 +20,7 @@ const Dashboard = () => {
     <section>
       <h1>Admin Dasboard</h1>
       <div  className="form_container">
-        <form onSubmit={handleSubmit()} className='form' >
+        <form onSubmit={handleSubmit(createEntry)} className='form' >
         <input  {...register("image")} type="text" name="image" id="image" placeholder="Url imagen de portada"/>
           <input  {...register("title")} type="text" name="title" id="title" placeholder="Titulo del Blog"/>
           <textarea  {...register("content")} name="content" id="content" cols="30" rows="10" placeholder="Blog content" />
