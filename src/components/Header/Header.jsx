e16a18351fc64c28f88bb39cb1fe545fd11e3a39
alenriquez96 <alberto.enriquez96@gmail.com>
2022-05-21 18:57:30 +0200

    added new popup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Nav from "./Nav";
 import {Link}from "react-router-dom";
 import loginLogo from "../../assets/acc-circle.png";
@@ -6,26 +6,41 @@ import voluntariadoLogo from "../../assets/voluntariado-logo.png";
 import logoGrande from "../../assets/logoGrande.png"
 import axios from "axios";
 import { useForm } from "react-hook-form";
-import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import Dialog from '@mui/material/Dialog';
+
 
 
 
 const Header = () => {
   const { register, handleSubmit } = useForm();
+  const [open, setOpen] = useState(false)
 
   const loginUser = async(log)=>{
     const res = await axios.post("http://localhost:5000/api/users/login",log);
     const data = res.data;
     console.log(data);
+    localStorage.setItem("token",data.token);
     if(data.message==='Correct credentials'){
     }
   }
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <header>
       <div id="divLogos">
       <Link to="/maintenance"><img src={voluntariadoLogo} alt="voluntariado logo"/></Link>
-        <Popup trigger={<img src={loginLogo} alt="login logo"/>} position="left top">
+      <img onClick={handleOpen}src={loginLogo} alt="login logo" />
+          
+      </div>
+      <Dialog onClose={handleClose} open={open}>
           <form className="formPopup" onSubmit={handleSubmit(loginUser)}>
             <img src={logoGrande} alt="logoGrande" />
             <h2>Empezar</h2>
@@ -35,10 +50,9 @@ const Header = () => {
             <label htmlFor="pass1">Contraseña</label>
             <input {...register("pass1")} name="pass1" type="password"/>
             <button className="mainBtn" type="submit">Iniciar Sesión</button>
-            <button className="secondBtn"><Link to="/signup">Hazte miembro</Link></button>
+            <button onClose={handleClose} className="secondBtn"><Link to="/signup">Hazte miembro</Link></button>
           </form>
-        </Popup>
-      </div>
+      </Dialog>
       <Nav/>
     </header>
   )
