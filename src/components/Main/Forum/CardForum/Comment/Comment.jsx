fa392c6d32c6ad37b4bf2e82f11039f6a55f46bf
alenriquez96 <alberto.enriquez96@gmail.com>
2022-05-23 21:31:16 +0200

    added redirect

--- src/components/Main/Forum/CardForum/Comment/Comment.jsx
+++ src/components/Main/Forum/CardForum/Comment/Comment.jsx
@@ -1,37 +1,22 @@
-import React, { useContext, useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-
+import React from "react";
 
 function Comment(props) {
 
-
-const commentdata= props.commentdata
-const [inputname, setSearchName] = useState([]);
-
-
-// const onSubmit = (e) => {
-//   e.preventDefault();
-//   if(e.target.nombre.value){
-//     setSearchName(e.target.nombre.value)
-//     console.log("llega masa",e.target.nombre.value); 
-//     const res = await axios.put(`http://localhost:5000/api/users/foro/${id}`,e.target.nombre.value);
-//   }}
-
+  const commentdata = props.commentdata
 
 
   return (
     <div className="card">
-       
-      
-   
-        <p>Comentarios:{commentdata.answer}</p>
-     
-   
 
 
-      </div>
-   
+
+      <p>Comentarios:{commentdata.answer}</p>
+
+
+
+
+    </div>
+
   );
 }
 
