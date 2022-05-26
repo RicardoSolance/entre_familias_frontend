4600a154c0c8d5517cb800bfe718bfbb922c2b75
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-05-26 23:22:44 +0200

    added routing

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,8 +3,10 @@ import Home from "./Home";
 import Forum from "./Forum";
 import Blog from "./Blog";
 import Dashboard from "./Dashboard/Dashboard";
-
+import Maintenance from "./Maintenance";
 import Profile from './Profile/Profile';
+import Privacy_politics from "./Privacy_politics";
+import Cookies_politics from "./cookies_politics";
 import {Route, Routes} from "react-router-dom";
 
 const Main = () => {
@@ -15,7 +17,9 @@ const Main = () => {
         <Route element={<Forum/>} path="/forum"/>
         <Route element={<Blog/>} path="/blog"/>
         <Route element={<Dashboard/>} path="/dashboard"/>
-  
+        <Route element={<Maintenance/>} path="/maintenance"/>
+        <Route element={<Privacy_politics/>} path="/privacy"/>
+        <Route element={<Cookies_politics/>} path="/cookies"/>
         <Route element={<Profile/>} path="/profile"/>
       </Routes>
     </main>
